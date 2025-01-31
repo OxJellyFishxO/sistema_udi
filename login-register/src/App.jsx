4f36ff878a51d88d25af857b92eb695d9d159a9e
Jovany Lugo Moreno <98961354+OxJellyFishxO@users.noreply.github.com>
2025-01-30 20:51:55 -0600

    Initial commit

---
+++ login-register/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { ssrImportKey } from 'vite/module-runner';
+import 'style/tailwind/tailwind.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold underline'> Registro </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
