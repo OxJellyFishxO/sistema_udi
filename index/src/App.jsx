e0dc6735d2775a4063bbf79ace248d1c2a26f693
Jovany Lugo Moreno <98961354+OxJellyFishxO@users.noreply.github.com>
2025-02-12 20:50:48 -0600

    React-Router dentro del Index

--- index/src/App.jsx
+++ index/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import IndexPage from './pages/IndexPage'
 import './assets/styles/tailwind/tailwind.css'
 import './assets/styles/style/style.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router'
 
 function IndexApp() {
-
     return (
         <>
-            <IndexPage />
+            <Router>
+                <Routes>
+                    <Route path='/' element={<IndexPage />} ></Route>
+                </Routes>
+            </Router>
         </>
     )
 }
