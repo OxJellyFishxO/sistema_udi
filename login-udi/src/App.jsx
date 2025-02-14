7b0e630b4f541722da02c3ac5792b00f276efd80
Jovany Lugo Moreno <98961354+OxJellyFishxO@users.noreply.github.com>
2025-02-14 13:18:03 -0600

    Login-UDI

---
+++ login-udi/src/App.jsx
@@ -0,0 +1,12 @@
+import './assets/styles/tailwind/tailwind.css';
+import React from "react";
+
+function LoginPageUDI() {
+    return (
+        <>
+            <div> <h1>Login de UDI</h1></div>
+        </>
+    )
+}
+
+export default LoginPageUDI;
